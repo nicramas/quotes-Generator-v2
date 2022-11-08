46b5346ce2caf5a12e2ab78e88e9076d6f374da0
Marcin Sapierzyński <m.sapierzynski@hotmail.com>
2022-11-08 20:56:21 +0100

    scss done

---
+++ script.js
@@ -0,0 +1,51 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote_text_content');
+const quoteAuthor = document.getElementById('quote_author_name');
+const copyQuoteBtn = document.getElementById('copy-quote');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+//Show new Quote
+function newQuote() {
+    //Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    console.log(quote);
+
+    //Check if Author field is empty and replace it with 'Unknown'
+    if (!quote.author) {
+        quoteAuthor.textContent = 'Unknown';
+    } else {
+        quoteAuthor.textContent = quote.author;
+    }
+    //Check Quote length to determinate styling
+    if (quote.text.length > 150) {
+        quoteText.classList.add("long-quote");
+    } else {
+        quoteText.classList.remove("long-quote");
+    }
+    //Set Quote
+    quoteText.textContent = quote.text;
+}
+
+//Get Quotes from API
+const API = "https://type.fit/api";
+
+function getQuotes() {
+fetch(`${API}/quotes`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    newQuote();
+}
+
+//Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+
+
+getQuotes();
+
